3d7d60f92b4bff0a102c637b135b2961ca11c0f0
shenjianpeng <335999189@qq.com>
2019-10-25 23:54:21 +0800

    test

--- utils/tushare_helper.py
+++ utils/tushare_helper.py
@@ -2,6 +2,7 @@
 import tushare as ts
 from datetime import datetime, date, time
 import pandas as pd
+import time as tt
 
 
 # 单例
@@ -25,5 +26,12 @@ def pro_adj_bar(ts_code='', start_date='', end_date='', freq='D'):
 
 
 if __name__ == '__main__':
-    df = TushareApi.pro.top10_holders(ts_code='600000.SH', start_date='20190101', end_date='20191231')
-    df.to_csv("abc.csv")
+    stockCodeList = TushareApi.pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+    list = []
+    for index, row in stockCodeList.head(100).iterrows():
+        df = TushareApi.pro.top10_holders(ts_code=row['ts_code'], start_date='20190101', end_date='20191231')
+        df = df.head(10)[df.head(10)['holder_name'].str.contains(pat = '中国证券金融股份有限公司')]
+        df.empty or list.append(df)
+        (index+1) % 78 == 0 and tt.sleep(60)
+    dd = pd.concat(list,ignore_index=True)
+    dd.to_csv("abc.csv")
